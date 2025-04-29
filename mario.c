4d54d5f101b8cbf463720d7ffb12bae691cf3c83
Parham Safiyari <safiyari18@gmail.com.com>
2025-04-29 18:43:53 +0330

    adding mario project

---
+++ mario.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int n, int bricks);
+
+int main(void)
+{
+    // Prompt the user for the pyramid's height
+    int n;
+
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+
+    // Print a pyramid of that height
+    for (int i = 0; i < n; i++)
+    {
+        // Print each row
+        print_row(n, i + 1);
+    }
+}
+
+void print_row(int height, int bricks)
+{
+    // Print spaces
+    for (int i = 0; i < height - bricks; i++)
+    {
+        printf(" ");
+    }
+
+    // Print bricks
+    for (int j = 0; j < bricks; j++)
+    {
+        printf("#");
+    }
+
+    // Move to the next line
+    printf("\n");
+}
